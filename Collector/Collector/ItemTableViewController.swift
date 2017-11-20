453f0a0711bc98c8e4ac52b576f774299d0a4908
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2017-11-19 22:50:24 -0600

    App is fully functional. Can add items, then remove them, close app and persist data

--- Collector/Collector/ItemTableViewController.swift
+++ Collector/Collector/ItemTableViewController.swift
@@ -38,9 +38,8 @@ class ItemTableViewController: UITableViewController {
     }
 
     
-    // Override to support conditional editing of the table view.
+
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
     
@@ -52,11 +51,12 @@ class ItemTableViewController: UITableViewController {
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+          // this is where the delete happens for the table cell
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                context.delete(collectorItems[indexPath.row])
+                getItemsFromCoreData()
+            }
+        }
     }
     
     //MARK: functions
